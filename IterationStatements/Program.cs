4e642d6db01c65b8f04a19d541de5d8b35688c10
Michael Cerase <michaelcerase@gmail.com>
2020-01-22 12:36:59 -0600

    Tasks done for while loops

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IterationStatements
 {
@@ -8,32 +9,49 @@ namespace IterationStatements
         static void Main(string[] args)
         {
             //TODO
+            
 
-            //Create a List called "numbers"
-            //Create a variable of type int with an initializer of 0
+            //DONE - Create a List called "numbers"
+            var numbers = new List<int>();
+            //DONE -Create a variable of type int with an initializer of 0
+            var num = 0;
 
-
-
+            string[] arr = new string[10];
+            string s = String.Join("", arr);
             // Create a do-while loop
+            do
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
-            // While your variable is less than 100
+
+                // DONE - Increment your variable by 1
+                num++;
+                // DONE - Then add your variable to "numbers"
+                numbers.Add(num);
+
+            } while (num < 100);
+            // DONE - While your variable is less than 100
 
 
 
-            // Create a while loop
-            // While your variable is less than 200
+            // DONE - Create a while loop
+            // DONE - While your variable is less than 200
+            while(num < 200)
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+
+                // DONE - Increment your variable by 1
+                num++;
+                // DONE - Then add your variable to "numbers"
+                numbers.Add(num);
+
             }
 
             Console.WriteLine("Increase:");
 
-            // Create a foreach loop
-            // Write your variable to the console
+            // DONE - Create a foreach loop
+            // DONE - Write your variable to the console
+            foreach(int a in numbers)
+            {
+                Console.WriteLine(a);
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -43,8 +61,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for(int i = 199; i <= numbers.ToArray().Length && i >= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine(numbers[i]);
             }
         }
     }
